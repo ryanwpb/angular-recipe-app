b13a5929f8c803850e1c07a16a7f3a0cb272af1c
Ryan Graham <ryangrahamwpb@gmail.com>
2020-06-22 11:40:25 -0400

    Render recipe data from recipe model.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipies = [];
+  recipies: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'This is the description',
+      'https://i.pinimg.com/originals/79/2c/8c/792c8c2f72c6fb8f542a0829d3ff6466.png'
+    ),
+  ];
   constructor() {}
 
   ngOnInit(): void {}
